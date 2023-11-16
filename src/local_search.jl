71ba16d31f0c1f6e8deae2ddcfabd17f81cfa694
NIkita <nikita.pomies@live.fr>
2023-11-16 11:04:21 +0100

    cleaner and faster recherche function

--- src/local_search.jl
+++ src/local_search.jl
@@ -1,44 +1,61 @@
 
-function customize_argmin(M::Matrix{Int}, skip::Matrix{Int64}, S::Solution,current_iteration::Int)
-    min = 100000000
-    min_row_idx = -1
-    min_col_idx = -1
-    num_rows = size(M, 1)
-    num_cols = size(M, 2)
 
-    for i in 1:num_rows
-        for j in 1:num_cols
-            if skip[i,j] > current_iteration || (j == S.coloring.colors[i])
+function customize_argmin(M::Matrix{Int}, tabu_matrix::Matrix{Int64},S::Solution,current_iteration::Int)
+    min_val_with_tabu = Inf
+    min_val_without_tabu = Inf
+    indices_with_tabu = []
+    indices_without_tabu = []
+    
+    for i in 1:size(M, 1)
+        for j in 1:size(M, 2)
+            if j == S.coloring.colors[i]
                 continue
-            else
-                if M[i, j] < min
-                    min_row_idx = i
-                    min_col_idx = j
-                    min = M[i, j]
-                end
+            end
 
+            if tabu_matrix[i,j] <= current_iteration
+                if M[i, j] < min_val_without_tabu
+                    min_val_without_tabu = M[i, j]
+                    indices_without_tabu = [(i, j)]
+                elseif M[i, j] == min_val_without_tabu
+                    push!(indices_without_tabu, (i, j))
+                end
+            end
+            
+            if M[i, j] < min_val_with_tabu
+                min_val_with_tabu = M[i, j]
+                indices_with_tabu = [(i, j)]
+            elseif M[i, j] == min_val_with_tabu
+                push!(indices_with_tabu, (i, j))
             end
         end
     end
-    return min_row_idx, min_col_idx,min
-
+    
+    random_index_with_tabu = rand(1:length(indices_with_tabu))
+    random_index_without_tabu = rand(1:length(indices_without_tabu))
+    
+    return ((indices_with_tabu[random_index_with_tabu],min_val_with_tabu),
+    (indices_without_tabu[random_index_without_tabu],min_val_without_tabu))
 end
 
 
 function LS_best_1opt!(S::Solution,best_found_val::Int,current_iteration::Int,plateau_counter::Int)::Bool
     
-    index = customize_argmin(S.T, zeros(Int,nv(S.graph),length(S.coloring.colors)), S,current_iteration)
-    v, new_color,obj_diff = index[1], index[2],index[3]
+    index_with_tabu,index_no_tabu = customize_argmin(S.T, S.TabuMatrix, S,current_iteration)
+
+    v_tabu, new_color_tabu,obj_diff_tabu = index_with_tabu[1][1], index_with_tabu[1][2],index_with_tabu[2]
+    v_no_tabu, new_color_no_tabu,obj_diff_no_tabu = index_no_tabu[1][1], index_no_tabu[1][2],index_no_tabu[2]
+
     # Aspiration criteria
-    if !(S.cost + obj_diff < best_found_val)
-        index = customize_argmin(S.T, S.TabuMatrix, S, current_iteration)
-        v, new_color,obj_diff = index[1], index[2],index[3]
+    if (S.cost + obj_diff_no_tabu < best_found_val)
+        print("ASPIRATION !!!")
+        v, new_color,obj_diff =v_tabu,new_color_tabu,obj_diff_tabu
     else
-        print("ASPIRATION !!")
+        v, new_color,obj_diff =v_no_tabu,new_color_no_tabu,obj_diff_no_tabu
+        
     end
     old_color = S.coloring.colors[v]    
     S.coloring.colors[v] = new_color
-    S.TabuMatrix[v, old_color] = current_iteration + 20 +  round(Int,S.cost) + div(plateau_counter,1000)
+    S.TabuMatrix[v, old_color] = current_iteration + 10 +  round(Int,S.cost) + div(plateau_counter,1000)
     S.cost += S.T[v,new_color]
     update_color_change_table!(v, old_color, new_color, S)
     return true
@@ -58,7 +75,7 @@ function TabuSearch(S::Solution)
     global plateau_counter = 0
     global previous_cost = S.cost
     
-    while (iteration < 100000 && best_found_val!=0)
+    while (iteration < 200000 && best_found_val!=0)
 
         println("Iteration $(iteration)")
         improved = LS_best_1opt!(S,best_found_val,iteration,plateau_counter)
@@ -71,7 +88,7 @@ function TabuSearch(S::Solution)
         end
 
         if improved
-            println("Cost after best 1opt of iteration $(iteration) $(coloring_cost(S)) $(S.cost)")
+            println("Cost after best 1opt of iteration $(iteration) $(S.cost)")
             if S.cost < best_found_val
                 best_found_sol = deepcopy(S)
                 best_found_val = S.cost
